74df066072ec556189b686b530dfb9855e18a487
Ghost_Boy <129123736+Hacker273020@users.noreply.github.com>
2024-04-23 12:28:02 -0600

    bruteforce.py

--- bruteforce.py
+++ bruteforce.py
@@ -1,13 +1,13 @@
-""" si quieres realizar un ataque de fuerza bruta y tienes el usuario 
-y te falta la contraseña estas en el lugar indicado
-solo en """
-try_login(url, 'admin', password):
-""" hay que cambiar el usuario que en este caso
-es 'admin' pero tu lo puedes modificar por cualquier usuario
-y cuando te pide el diccionario tienes que poner la ubicacion de tu diccionario txt
-en mi caso les recomiendo usar el diccionario o wordlist Rockyou.txt 
-"""
-# si quieres mas ayuda con cualquier tipo de ataque mandame msg en instagram ezemtz.2222
+# si quieres realizar un ataque de fuerza bruta y tienes el usuario 
+# y te falta la contraseña estas en el lugar indicado
+# solo en 
+# try_login(url, 'admin', password):
+# hay que cambiar el usuario que en este caso
+# es 'admin' pero tu lo puedes modificar por cualquier usuario
+# y cuando te pide el diccionario tienes que poner la ubicacion de tu diccionario txt
+# en mi caso les recomiendo usar el diccionario o wordlist Rockyou.txt 
+
+# si quieres mas ayuda con cualquier tipo de ataque mandame msg a mi instagram ezemtz.2222
 
 import requests
 
